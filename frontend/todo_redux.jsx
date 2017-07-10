3fae2fd74c7311ca440674c8c687a97066da0f53
Howard Feger <hfeger4@gmail.com>
2017-07-10 11:15:47 -0700

    Created skeleton for React Redux project

---
+++ frontend/todo_redux.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// <h1>Todos App</h1>
+
+
+document.addEventListener("DOMContentLoaded", () => {
+	ReactDOM.render(
+		<h1>Todos App</h1>,
+		document.getElementById('root')
+	);
+});
